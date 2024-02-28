1de2220f0c9b25a3e78a48a4585c5882e52ac1ba
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-02-28 21:40:39 +0800

    css

--- includes/learndash.php
+++ includes/learndash.php
@@ -91,8 +91,9 @@ function _learndash_linked_product()
     if ($products) {
         if (count($products) == 1) {
 
-            $html .= '<a href="/shop/?add-to-cart=' . $products[0]->ID . '" data-quantity="1" class="button product_type_course add_to_cart_button ajax_add_to_cart" data-product_id="' . $products[0]->ID . '"  aria-describedby="" rel="nofollow">Add to basket</a>';
-
+            $html .= '<a href="/shop/?add-to-cart=' . $products[0]->ID . '" data-quantity="1" class="button product_type_course add_to_cart_button ajax_add_to_cart" data-product_id="' . $products[0]->ID . '"  aria-describedby="" rel="nofollow">Add to basket';
+            $html .= '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/> <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/> </svg>';
+            $html .= '</a>';
 
             return $html;
         }
