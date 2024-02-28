dc8c6b48d2e0861f8a94d67848a336b28665f902
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-02-28 14:28:15 +0800

    css

--- includes/learndash.php
+++ includes/learndash.php
@@ -19,8 +19,25 @@ function _learndash_course_meta()
 add_shortcode('_learndash_course_meta', '_learndash_course_meta');
 
 
-function _learndash_status_bubble() {
+function _learndash_status_bubble()
+{
     $course_status = learndash_course_status(get_the_ID());
     return learndash_status_bubble($course_status);
 }
 add_shortcode('_learndash_status_bubble', '_learndash_status_bubble');
+
+
+function learndash_wp_footer()
+{
+    if (get_post_type() == 'sfwd-courses') {
+?>
+        <script>
+            jQuery(document).ready(function() {
+                jQuery('.ld-progress-steps').appendTo('.learndash-wrapper');
+            });
+        </script>
+<?php
+    }
+}
+
+add_action('wp_footer', 'learndash_wp_footer');
