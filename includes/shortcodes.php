11f04d0ab056d235153f55212d195029c09ad624
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-02-28 16:16:30 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -4,6 +4,11 @@ function breadcrumbs()
     $html = '<div class="breadcrumbs-holder">';
     $html .= '<ul>';
     $html .= '<li><a href="' . get_site_url() . '">Home</a></li>';
+
+    if (is_post_type_archive()) {
+        $html .= '<li><a href="' . get_site_url() . '">'.get_the_archive_title().'</a></li>';
+    }
+
     $html .= '</ul>';
     $html .= '</div>';
     return $html;
