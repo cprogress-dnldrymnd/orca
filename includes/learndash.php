29f57a57f1f9c4bbd7760f13f33fc087dd095e86
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-02-28 18:02:32 +0800

    Update learndash.php

--- includes/learndash.php
+++ includes/learndash.php
@@ -1,13 +1,10 @@
 <?php
 function _learndash_course_progress()
 {
-    return do_shortcode('[learndash_course_progress user_id="' . get_current_user_id() . '"]');
+    return do_shortcode('[learndash_course_progress course_id="' . get_the_ID() . '"]');
 }
 
 add_shortcode('_learndash_course_progress', '_learndash_course_progress');
-
-
-
 
 
 function _learndash_course_meta()
