ab234baaafe1d4bb289d8d007dc267b6a11b56f1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-02-29 18:58:18 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -1,10 +1,10 @@
 <?php
 function filter_woocommerce_cart_redirect_after_error($redirect, $product_id)
 {
-    $_related_course = get_post_meta($product_id, '_related_course');
+    $_related_course = get_post_meta($product_id, '_related_course', true);
 
     if (count($_related_course) == 1) {
-        $redirect = get_the_permalink($_related_course[0]);
+        $redirect = esc_url(get_the_permalink($_related_course[0]));
     } else {
         $redirect = esc_url(WC()->cart->get_cart_url());
     }
