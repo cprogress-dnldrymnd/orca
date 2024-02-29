0712512fc820d846c3988f46cda4e36f91901661
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-02-29 18:59:30 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -8,7 +8,6 @@ function filter_woocommerce_cart_redirect_after_error($redirect, $product_id)
     } else {
         $redirect = esc_url(WC()->cart->get_cart_url());
     }
-    $redirect = esc_url(WC()->cart->get_cart_url());
 
     return $redirect;
 }
