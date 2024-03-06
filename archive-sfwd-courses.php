7c8abec57dea700284334f0726990c18d2008042
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-07 03:52:02 +0800

    css

--- archive-sfwd-courses.php
+++ archive-sfwd-courses.php
@@ -7,14 +7,18 @@
             <?php while (have_posts()) { ?>
                 <?php the_post() ?>
                 <div class="col-lg-4">
-                    <div class="column-holder background-white h-100">
+                    <div class="column-holder d-flex flex-column justify-content-end background-white h-100">
                         <?= do_shortcode('[_learndash_image id="' . get_post_thumbnail_id() . '" size="medium" learndash_status_bubble="true" taxonomy="ld_course_category"]') ?>
-                        <div class="content-holder">
-                            <?= do_shortcode('[_heading tag="h3" heading="' . get_the_title() . '"]'); ?>
-                            <?= do_shortcode('[_description description="If you want to get onboard and become one of our volunteer Marine Mammal Surveyors..."]'); ?>
-                            <hr>
-                            <?= do_shortcode('[_learndash_course_meta]'); ?>
-                            <?= do_shortcode('[_learndash_course_button]'); ?>
+                        <div class="content-holder d-flex flex-column justify-content-end">
+                            <div>
+                                <?= do_shortcode('[_heading tag="h3" heading="' . get_the_title() . '"]'); ?>
+                                <?= do_shortcode('[_description description="If you want to get onboard and become one of our volunteer Marine Mammal Surveyors..."]'); ?>
+                                <hr>
+                                <?= do_shortcode('[_learndash_course_meta]'); ?>
+                            </div>
+                            <div>
+                                <?= do_shortcode('[_learndash_course_button]'); ?>
+                            </div>
                         </div>
                     </div>
                 </div>
