51ae8e0ff208d75dd33cfe89b7159c2fce755856
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-06 18:19:06 +0800

    Update learndash.php

--- includes/learndash.php
+++ includes/learndash.php
@@ -83,26 +83,24 @@ function learndash_wp_footer()
                 lastScrollTop = st;
             });
 
-            jQuery(document).ready(function() {
-                var acc = document.getElementsByClassName(".ld-item-list-section-heading");
-                var i;
+            var acc = document.getElementsByClassName("ld-item-list-section-heading");
+            var i;
 
-                for (i = 0; i < acc.length; i++) {
-                    acc[i].addEventListener("click", function() {
-                        /* Toggle between adding and removing the "active" class,
-                        to highlight the button that controls the panel */
-                        this.classList.toggle("active");
+            for (i = 0; i < acc.length; i++) {
+                acc[i].addEventListener("click", function() {
+                    /* Toggle between adding and removing the "active" class,
+                    to highlight the button that controls the panel */
+                    this.classList.toggle("active");
 
-                        /* Toggle between hiding and showing the active panel */
-                        var panel = this.nextElementSibling;
-                        if (panel.style.display === "block") {
-                            panel.style.display = "none";
-                        } else {
-                            panel.style.display = "block";
-                        }
-                    });
-                }
-            });
+                    /* Toggle between hiding and showing the active panel */
+                    var panel = this.nextElementSibling;
+                    if (panel.style.display === "block") {
+                        panel.style.display = "none";
+                    } else {
+                        panel.style.display = "block";
+                    }
+                });
+            }
         </script>
 <?php
     }
