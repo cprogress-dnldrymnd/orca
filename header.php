0d3c6460071b49497c590ec8989678b0a8142284
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-06 23:53:00 +0800

    Update header.php

--- header.php
+++ header.php
@@ -24,28 +24,33 @@
     <div id="page" class="site">
 
         <header id="masthead" class="site-header">
-            <div class="container">
-                <nav class="navbar navbar-expand-md navbar-light bg-light">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">Navbar</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div class="collapse navbar-collapse" id="main-menu">
-                            <?php
-                            wp_nav_menu(array(
-                                'theme_location' => 'header',
-                                'container' => false,
-                                'menu_class' => '',
-                                'fallback_cb' => '__return_false',
-                                'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
-                                'depth' => 2,
-                                'walker' => new bootstrap_5_wp_nav_menu_walker()
-                            ));
-                            ?>
+            <nav class="navbar navbar-expand-md navbar-light bg-light">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-auto">
+                            <div class="navbar-brand">
+                                <?= get_custom_logo() ?>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <a class="navbar-brand" href="#">Navbar</a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="main-menu">
+                                <?php
+                                wp_nav_menu(array(
+                                    'theme_location' => 'header',
+                                    'container' => false,
+                                    'menu_class' => '',
+                                    'fallback_cb' => '__return_false',
+                                    'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
+                                    'depth' => 2,
+                                ));
+                                ?>
+                            </div>
                         </div>
                     </div>
-                </nav>
-            </div>
+                </div>
+            </nav>
         </header><!-- #masthead -->
