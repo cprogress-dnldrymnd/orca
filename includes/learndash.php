8c6374114faf323be3d71bdb8c6a4b0f529858cd
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-06 17:14:47 +0800

    css

--- includes/learndash.php
+++ includes/learndash.php
@@ -178,8 +178,9 @@ function new_default_content($content)
 }
 add_filter('the_content', 'new_default_content', 9999);
 
+/*
 function action_learndash_focus_header_before() {
     echo do_shortcode('[elementor-template id="122"]');
 }
 
-add_action('learndash-focus-header-before', 'action_learndash_focus_header_before');
+add_action('learndash-focus-header-before', 'action_learndash_focus_header_before');*/
