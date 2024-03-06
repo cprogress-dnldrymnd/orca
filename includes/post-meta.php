2cdcd7289a58773daf7768e8e238e5d1902ee81b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-06 20:01:32 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -9,13 +9,24 @@ use Carbon_Fields\Field;
 /* Courses
 /*-----------------------------------------------------------------------------------*/
 
-Container::make('post_meta', 'Download Guide')
-	->where('post_type', '=', 'sfwd-courses')
-	->set_priority('high')
-    
-	->add_fields(
-		array(
-			Field::make('text', 'text2', __('text')),
+Container::make('post_meta', 'Course settings')
+    ->where('post_type', '=', 'sfwd-courses')
+    ->set_priority('high')
 
-		)
-	);
+    ->add_fields(
+        array(
+            Field::make('text', 'text2', __('text')),
+
+        )
+    );
+
+Container::make('post_meta', 'Outcomes')
+    ->where('post_type', '=', 'sfwd-courses')
+    ->set_priority('high')
+
+    ->add_fields(
+        array(
+            Field::make('text', 'text2', __('text')),
+
+        )
+    );
