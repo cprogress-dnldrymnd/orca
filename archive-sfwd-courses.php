588f2f444b8e640535bdf7607990e73f263176ff
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-07 03:52:37 +0800

    css

--- archive-sfwd-courses.php
+++ archive-sfwd-courses.php
@@ -7,9 +7,9 @@
             <?php while (have_posts()) { ?>
                 <?php the_post() ?>
                 <div class="col-lg-4">
-                    <div class="column-holder d-flex flex-column justify-content-end background-white h-100">
+                    <div class="column-holder d-flex flex-column justify-content-between background-white h-100">
                         <?= do_shortcode('[_learndash_image id="' . get_post_thumbnail_id() . '" size="medium" learndash_status_bubble="true" taxonomy="ld_course_category"]') ?>
-                        <div class="content-holder d-flex flex-column justify-content-end">
+                        <div class="content-holder d-flex flex-column justify-content-between">
                             <div>
                                 <?= do_shortcode('[_heading tag="h3" heading="' . get_the_title() . '"]'); ?>
                                 <?= do_shortcode('[_description description="If you want to get onboard and become one of our volunteer Marine Mammal Surveyors..."]'); ?>
