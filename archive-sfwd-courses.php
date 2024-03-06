57a0ede70088dd52924ad054101d81c73d4ceb72
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-07 02:51:22 +0800

    cs

--- archive-sfwd-courses.php
+++ archive-sfwd-courses.php
@@ -10,8 +10,8 @@
                     <div class="column-holder">
                         <?= do_shortcode('[_learndash_image id="' . get_post_thumbnail_id() . '" size="medium" learndash_status_bubble="true" taxonomy="ld_course_category"]') ?>
                         <div class="content-holder">
-                            <?= do_shortcode('[_heading heading="' . get_the_title() . '" ]'); ?>
-                            <?= do_shortcode('[_description description="' . get_the_excerpt() . '" ]'); ?>
+                            <?= do_shortcode('[_heading heading="' . get_the_title() . '"]'); ?>
+                            <?= do_shortcode('[_description description="' . get_the_excerpt() . '"]'); ?>
                             <hr>
                             <?= do_shortcode('[_learndash_course_meta]'); ?>
                             <?= do_shortcode('[_learndash_course_button]'); ?>
