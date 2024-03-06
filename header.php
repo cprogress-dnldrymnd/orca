150f6c2dec99ad157bd6676dfebb57745bb5961e
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-07 06:46:27 +0800

    Update header.php

--- header.php
+++ header.php
@@ -13,8 +13,14 @@
 
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <title>
+        <?php bloginfo('name'); // show the blog name, from settings 
+        ?> |
+        <?php is_front_page() ? bloginfo('description') : wp_title(''); // if we're on the home page, show the description, from the site's settings - otherwise, show the title of the post or page 
+        ?>
+    </title>
     <?php wp_head(); ?>
 </head>
 
