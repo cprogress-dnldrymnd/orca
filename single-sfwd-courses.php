895e387247df3b4a780eee59411e8ff2c2700170
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-07 05:48:13 +0800

    css

--- single-sfwd-courses.php
+++ single-sfwd-courses.php
@@ -8,12 +8,14 @@
             </div>
             <div class="single-course-content-holder background-white pt-3">
                 <div class="learndash-single-holder learndash-single-status-top">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <?= do_shortcode('[_learndash_course_progress]') ?>
-                        </div>
-                        <div class="col-md-4">
-                            <?= do_shortcode('[_learndash_status]') ?>
+                    <div class="inner background-light-gray">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <?= do_shortcode('[_learndash_course_progress]') ?>
+                            </div>
+                            <div class="col-md-4">
+                                <?= do_shortcode('[_learndash_status]') ?>
+                            </div>
                         </div>
                     </div>
                 </div>
