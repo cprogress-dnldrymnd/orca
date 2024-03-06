68944a819683b2bbf5cf2fe375fd985d47f417b0
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-07 02:31:44 +0800

    cs

--- archive-sfwd-courses.php
+++ archive-sfwd-courses.php
@@ -9,7 +9,7 @@
                 <div class="col-lg-4">
                     <div class="column-holder">
                         <?php
-                        echo do_shortcode('[_image id="' . get_post_thumbnail_id() . '" ]');
+                        echo do_shortcode('[_image id="' . get_post_thumbnail_id() . ' size="medium" ]');
                         echo do_shortcode('[_heading heading="' . get_the_title() . '" ]');
                         ?>
                     </div>
