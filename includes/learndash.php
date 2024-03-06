588cb49a516e55d0c9dea3e2cfaf99f52e18eb28
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-06 18:16:44 +0800

    css

--- includes/learndash.php
+++ includes/learndash.php
@@ -83,8 +83,24 @@ function learndash_wp_footer()
                 lastScrollTop = st;
             });
 
+            var acc = document.getElementsByClassName(".ld-item-list-section-heading");
+            var i;
 
-            
+            for (i = 0; i < acc.length; i++) {
+                acc[i].addEventListener("click", function() {
+                    /* Toggle between adding and removing the "active" class,
+                    to highlight the button that controls the panel */
+                    this.classList.toggle("active");
+
+                    /* Toggle between hiding and showing the active panel */
+                    var panel = this.nextElementSibling;
+                    if (panel.style.display === "block") {
+                        panel.style.display = "none";
+                    } else {
+                        panel.style.display = "block";
+                    }
+                });
+            }
         </script>
 <?php
     }
