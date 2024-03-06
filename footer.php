356ad2a7db89680e227a52124738a13bc98fe432
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-07 04:36:21 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -15,13 +15,29 @@
 <footer id="site-footer" class="site-footer background-primary">
     <div class="container">
         <div class="row">
-            <?php if (get_sidebar('footer_left')) { ?>
-                <div class="col-lg-4">
-                    <div class="footer-left-holder">
-                        <?php dynamic_sidebar('footer_left') ?>
-                    </div>
+            <div class="col-12">
+                <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'footer-main-menu',
+                    'container' => false,
+                    'menu_class' => '',
+                    'fallback_cb' => '__return_false',
+                    'items_wrap' => '<ul id="%1$s" class="menu" >%3$s</ul>',
+                    'depth' => 2,
+                ));
+                ?>
+            </div>
+            <div class="col-12">
+                <div class="row">
+                    <?php if (get_sidebar('footer_left')) { ?>
+                        <div class="col-lg-4">
+                            <div class="footer-left-holder">
+                                <?php dynamic_sidebar('footer_left') ?>
+                            </div>
+                        </div>
+                    <?php } ?>
                 </div>
-            <?php } ?>
+            </div>
         </div>
     </div>
 </footer>
