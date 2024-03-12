7272e3ff535d47742bef20f7bab5d9226549eb5a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-12 21:20:05 +0800

    css

--- single-sfwd-courses.php
+++ single-sfwd-courses.php
@@ -52,7 +52,7 @@
                     <div class="anchor-link" id="modules"></div>
                     <?= do_shortcode('[course_content]') ?>
                 </div>
-                <div class="learndash-single-holder learndash-single-testimonial position-relative background-dark py-4">
+                <div class="learndash-single-holder learndash-single-testimonial position-relative background-dark">
                     <div class="anchor-link" id="modules"></div>
                     <?= do_shortcode('[_course_testimonial]') ?>
                 </div>
