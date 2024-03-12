9edaf30cdd20193c29e1ac86ba845a608cbdd665
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-12 21:05:41 +0800

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,10 +1,12 @@
-var swiper = new Swiper(".swiper-testimonial", {
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+jQuery(document).ready(function () {
+    var swiper = new Swiper(".swiper-testimonial", {
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
 });
