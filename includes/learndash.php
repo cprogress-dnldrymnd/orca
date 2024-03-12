3d1c947a7986ba469e3b8ef009644b0d7f1570c1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-12 22:07:24 +0800

    Update learndash.php

--- includes/learndash.php
+++ includes/learndash.php
@@ -462,3 +462,27 @@ function _course_group()
 }
 
 add_shortcode('_course_group', '_course_group');
+
+
+add_action("pre_get_posts", "custom_front_page");
+function custom_front_page($wp_query){
+    //Ensure this filter isn't applied to the admin area
+    if(is_admin()) {
+        return;
+    }
+
+    if($wp_query->get('page_id') == get_option('page_on_front')):
+
+        $wp_query->set('post_type', 'courses');
+        $wp_query->set('page_id', ''); //Empty
+
+        //Set properties that describe the page to reflect that
+        //we aren't really displaying a static page
+        $wp_query->is_page = 0;
+        $wp_query->is_singular = 0;
+        $wp_query->is_post_type_archive = 1;
+        $wp_query->is_archive = 1;
+
+    endif;
+
+}
