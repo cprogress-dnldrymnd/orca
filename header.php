0a3cb1e187e42658053d67d88d655b6b96ec4add
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-18 21:10:14 +0800

    Update header.php

--- header.php
+++ header.php
@@ -39,14 +39,14 @@
                             <div class="header-right-top d-flex justify-content-end mb-0 mb-lg-3">
                                 <ul class="list-inline-icons list-inline d-inline-flex flex-wrap mb-0 align-items-center me-3 me-lg-0">
                                     <li>
-                                        <a href="">
+                                        <a href="<?= wc_get_cart_url() ?>">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket3" viewBox="0 0 16 16">
                                                 <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM3.394 15l-1.48-6h-.97l1.525 6.426a.75.75 0 0 0 .729.574h9.606a.75.75 0 0 0 .73-.574L15.056 9h-.972l-1.479 6z" />
                                             </svg>
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="">
+                                        <a href="<?= get_permalink(get_option('woocommerce_myaccount_page_id'));  ?>">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                                                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
                                             </svg>
