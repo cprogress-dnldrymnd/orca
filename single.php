fcf2dc754d67315d558743ea1075641111c0d835
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-20 01:03:44 +0800

    css

--- single.php
+++ single.php
@@ -3,7 +3,7 @@
     <div class="container large-container">
         <?php the_content() ?>
         <?php
-        var_dump(learndash_get_course_price(181)['price']);
+        var_dump(learndash_get_course_price(831)['price']);
         ?>
     </div>
 </section>
