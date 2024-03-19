bd73efe627c54f573062dcfca16a74e722497209
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-20 01:31:07 +0800

    Update single.php

--- single.php
+++ single.php
@@ -6,7 +6,12 @@
 
         $price = learndash_get_course_price(846)['price'];
         echo $price;
+        846
+
         ?>
+        <pre>
+            <?php var_dump(get_post_meta(846)) ?>
+        </pre>
     </div>
 </section>
 <?php get_footer() ?>
