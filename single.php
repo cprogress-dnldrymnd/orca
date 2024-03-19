b5a335118dde6d4018fb4246c7f122ba4c83bcc3
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-20 00:59:14 +0800

    Update single.php

--- single.php
+++ single.php
@@ -2,9 +2,9 @@
 <section class="archive-courses archive-grid background-light-gray py-5">
     <div class="container large-container">
         <?php the_content() ?>
-        <pre>
-         <?php var_dump(unserialize(get_post_meta(181)['_sfwd-courses'][0])['sfwd-courses_course_price']) ?>
-        </pre>
+        <?php
+        echo learndash_get_course_price(181);
+        ?>
     </div>
 </section>
 <?php get_footer() ?>
