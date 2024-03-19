11f3796ae91adfebd73f8540fcde4ed8cfc3e129
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-20 00:20:04 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -92,7 +92,8 @@ function bbloomer_save_name_fields($customer_id)
 }
 function so_post_40744782($new_status, $old_status, $post)
 {
-    if ($new_status == 'publish' && $old_status != 'publish') {
+    $post_type = get_post_type($post->ID);
+    if ($new_status == 'publish' && $old_status != 'publish' && $post_type =='sfwd-courses') {
         create_course_product($post->ID);
     }
 }
