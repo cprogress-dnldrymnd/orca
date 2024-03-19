a083e498305140b61049571355feda40209221bd
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-19 23:35:31 +0800

    Update single.php

--- single.php
+++ single.php
@@ -2,6 +2,9 @@
 <section class="archive-courses archive-grid background-light-gray py-5">
     <div class="container large-container">
         <?php the_content() ?>
+        <pre>
+         <?php var_dump(get_post_meta(get_the_ID())) ?>
+        </pre>
     </div>
 </section>
 <?php get_footer() ?>
