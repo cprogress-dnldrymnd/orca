23a6532f9034056bdc192214f904c3b0f2f10d0d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-20 02:26:24 +0800

    Update single.php

--- single.php
+++ single.php
@@ -9,11 +9,14 @@
         $product_price_update = get_option('product_price_update');
         if ($product_price_update) {
             foreach ($product_price_update as $product_id) {
+                unset($product_price_update[$product_id]);
                 $course_id = get_post_meta($product_id, '_related_course', true);
                 $price = learndash_get_course_price($course_id[0])['price'];
                 $product = new WC_Product_Course($product_id);
                 $product->set_regular_price($price);
                 $product->save();
+
+                update_option('product_price_update', $product_price_update);
             }
         }
 
