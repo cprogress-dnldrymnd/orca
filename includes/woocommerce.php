9bd3a2bf3f76c65e6a0eea5bdbd34e6e5fcea5c8
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-20 02:29:31 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -138,19 +138,3 @@ function product_save($post_id)
         $product->save();
     }
 }
-*/
-
-function action_admin_init()
-{
-    $product_price_update = get_option('product_price_update');
-    if ($product_price_update) {
-        foreach ($product_price_update as $product_id) {
-            $course_id = get_post_meta($product_id, '_related_course', true);
-            $price = learndash_get_course_price($course_id[0])['price'];
-            $product = new WC_Product_Course($product_id);
-            $product->set_regular_price($price);
-            $product->save();
-        }
-    }
-}
-add_action('init ', 'action_admin_init', 9999);
