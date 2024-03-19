43180e0cf95de72c431229d6bedddf8910063941
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-20 00:02:46 +0800

    Update single.php

--- single.php
+++ single.php
@@ -11,13 +11,18 @@ $product->set_regular_price(500.00); // in current shop currency
 
 $product->save();
 
-var_dump($product);
 ?>
+<pre>
+    <?php
+    var_dump($product);
+    ?>
+</pre>
 <section class="archive-courses archive-grid background-light-gray py-5">
     <div class="container large-container">
         <?php the_content() ?>
         <pre>
-         <?php //var_dump(get_post_meta(get_the_ID())) ?>
+         <?php //var_dump(get_post_meta(get_the_ID())) 
+            ?>
         </pre>
     </div>
 </section>
