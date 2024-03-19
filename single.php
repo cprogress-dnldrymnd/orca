bc3b3550960e351045caae3973e4398ff1f020c7
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-19 23:34:42 +0800

    Create single.php

---
+++ single.php
@@ -0,0 +1,7 @@
+<?php get_header() ?>
+<section class="archive-courses archive-grid background-light-gray py-5">
+    <div class="container large-container">
+        <?php the_content() ?>
+    </div>
+</section>
+<?php get_footer() ?>
