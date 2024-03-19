1007ea9051960772ac59f4c1f8640b818416c45a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-20 02:47:34 +0800

    css

--- single.php
+++ single.php
@@ -2,9 +2,6 @@
 <section class="archive-courses archive-grid background-light-gray py-5">
     <div class="container large-container">
         <?php the_content() ?>
-        <pre>
-            <?php var_dump(get_option('product_price_update')) ?>
-        </pre>
     </div>
 </section>
 <?php get_footer() ?>
