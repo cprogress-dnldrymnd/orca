744a05fed276cab7d6f19883ca9972b28e240020
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-20 00:01:25 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -15,9 +15,9 @@ add_filter('woocommerce_cart_redirect_after_error', 'filter_woocommerce_cart_red
 
 function searchfilter($query)
 {
-   
+
     //if ($query->is_search && !is_admin()) {
-     //   $query->set('post_type', array('post', 'sfwd-courses'));
+    //   $query->set('post_type', array('post', 'sfwd-courses'));
     //}
 
     return $query;
@@ -33,58 +33,66 @@ add_filter('pre_get_posts', 'searchfilter', 9999999);
  * @compatible    WooCommerce 8
  * @community     https://businessbloomer.com/club/
  */
- 
+
 ///////////////////////////////
 // 1. ADD FIELDS
- 
-add_action( 'woocommerce_register_form_start', 'bbloomer_add_name_woo_account_registration' );
- 
-function bbloomer_add_name_woo_account_registration() {
-    ?>
- 
+
+add_action('woocommerce_register_form_start', 'bbloomer_add_name_woo_account_registration');
+
+function bbloomer_add_name_woo_account_registration()
+{
+?>
+
     <p class="form-row form-row-first">
-    <label for="reg_billing_first_name"><?php _e( 'First name', 'woocommerce' ); ?> <span class="required">*</span></label>
-    <input type="text" class="input-text" name="billing_first_name" id="reg_billing_first_name" value="<?php if ( ! empty( $_POST['billing_first_name'] ) ) esc_attr_e( $_POST['billing_first_name'] ); ?>" />
+        <label for="reg_billing_first_name"><?php _e('First name', 'woocommerce'); ?> <span class="required">*</span></label>
+        <input type="text" class="input-text" name="billing_first_name" id="reg_billing_first_name" value="<?php if (!empty($_POST['billing_first_name'])) esc_attr_e($_POST['billing_first_name']); ?>" />
     </p>
- 
+
     <p class="form-row form-row-last">
-    <label for="reg_billing_last_name"><?php _e( 'Last name', 'woocommerce' ); ?> <span class="required">*</span></label>
-    <input type="text" class="input-text" name="billing_last_name" id="reg_billing_last_name" value="<?php if ( ! empty( $_POST['billing_last_name'] ) ) esc_attr_e( $_POST['billing_last_name'] ); ?>" />
+        <label for="reg_billing_last_name"><?php _e('Last name', 'woocommerce'); ?> <span class="required">*</span></label>
+        <input type="text" class="input-text" name="billing_last_name" id="reg_billing_last_name" value="<?php if (!empty($_POST['billing_last_name'])) esc_attr_e($_POST['billing_last_name']); ?>" />
     </p>
- 
+
     <div class="clear"></div>
- 
-    <?php
+
+<?php
 }
- 
+
 ///////////////////////////////
 // 2. VALIDATE FIELDS
- 
-add_filter( 'woocommerce_registration_errors', 'bbloomer_validate_name_fields', 10, 3 );
- 
-function bbloomer_validate_name_fields( $errors, $username, $email ) {
-    if ( isset( $_POST['billing_first_name'] ) && empty( $_POST['billing_first_name'] ) ) {
-        $errors->add( 'billing_first_name_error', __( '<strong>Error</strong>: First name is required!', 'woocommerce' ) );
+
+add_filter('woocommerce_registration_errors', 'bbloomer_validate_name_fields', 10, 3);
+
+function bbloomer_validate_name_fields($errors, $username, $email)
+{
+    if (isset($_POST['billing_first_name']) && empty($_POST['billing_first_name'])) {
+        $errors->add('billing_first_name_error', __('<strong>Error</strong>: First name is required!', 'woocommerce'));
     }
-    if ( isset( $_POST['billing_last_name'] ) && empty( $_POST['billing_last_name'] ) ) {
-        $errors->add( 'billing_last_name_error', __( '<strong>Error</strong>: Last name is required!.', 'woocommerce' ) );
+    if (isset($_POST['billing_last_name']) && empty($_POST['billing_last_name'])) {
+        $errors->add('billing_last_name_error', __('<strong>Error</strong>: Last name is required!.', 'woocommerce'));
     }
     return $errors;
 }
- 
+
 ///////////////////////////////
 // 3. SAVE FIELDS
- 
-add_action( 'woocommerce_created_customer', 'bbloomer_save_name_fields' );
- 
-function bbloomer_save_name_fields( $customer_id ) {
-    if ( isset( $_POST['billing_first_name'] ) ) {
-        update_user_meta( $customer_id, 'billing_first_name', sanitize_text_field( $_POST['billing_first_name'] ) );
-        update_user_meta( $customer_id, 'first_name', sanitize_text_field($_POST['billing_first_name']) );
+
+add_action('woocommerce_created_customer', 'bbloomer_save_name_fields');
+
+function bbloomer_save_name_fields($customer_id)
+{
+    if (isset($_POST['billing_first_name'])) {
+        update_user_meta($customer_id, 'billing_first_name', sanitize_text_field($_POST['billing_first_name']));
+        update_user_meta($customer_id, 'first_name', sanitize_text_field($_POST['billing_first_name']));
     }
-    if ( isset( $_POST['billing_last_name'] ) ) {
-        update_user_meta( $customer_id, 'billing_last_name', sanitize_text_field( $_POST['billing_last_name'] ) );
-        update_user_meta( $customer_id, 'last_name', sanitize_text_field($_POST['billing_last_name']) );
+    if (isset($_POST['billing_last_name'])) {
+        update_user_meta($customer_id, 'billing_last_name', sanitize_text_field($_POST['billing_last_name']));
+        update_user_meta($customer_id, 'last_name', sanitize_text_field($_POST['billing_last_name']));
     }
- 
+}
+
+add_action('woocommerce_admin_process_product_object', 'action_save_product_meta');
+function action_save_product_meta($product)
+{
+    $product->update_meta_data('_related_products', array(181));
 }
