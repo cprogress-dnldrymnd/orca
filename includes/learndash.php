903f1fceef40263e69bc35125ed601a9fbec864d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-11 23:56:12 +0800

    Update learndash.php

--- includes/learndash.php
+++ includes/learndash.php
@@ -276,7 +276,7 @@ add_shortcode('_learndash_image', '_learndash_image');
 function _learndash_course_button()
 {
     $permalink = get_the_permalink();
-    $html = '<div class="row g-3 button-group mt-3">';
+    $html = '<div class="row g-3 button-group">';
 
     $html .= '<div class="' . (_user_has_access() == false ? 'col-lg-6' : 'col-12') . '">';
     $html .= "<a  href='$permalink' class='btn btn-black w-100'>View Course</a>";
