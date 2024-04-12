0640937af5113cbd3bc1d4d661ea88f22a36c7a6
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-12 16:19:44 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ function action_wp_enqueue_scripts()
 	wp_enqueue_style('style', theme_dir . 'style.css');
 	wp_enqueue_script('bootstrap', vendor_dir . 'bootstrap/dist/js/bootstrap.min.js');
 	wp_register_script('swiper', vendor_dir . 'swiper/js/swiper-bundle.min.js');
-	if (is_post_type_archive('sfwd-courses')) {
+	if (is_post_type_archive('sfwd-courses') || is_tax('ld_course_category')) {
 		wp_enqueue_script('archive-course', assets_dir . 'javascripts/archive-course.js', array('jquery'));
 	} else if (is_single() && get_post_type() == 'sfwd-courses') {
 		wp_enqueue_script('single-course', assets_dir . 'javascripts/single-course.js', array('jquery', 'swiper'));
