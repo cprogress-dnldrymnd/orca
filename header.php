eca19c69642df9de4ac76f4119bcd52d425a5478
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-02 23:26:20 +0800

    Update header.php

--- header.php
+++ header.php
@@ -23,8 +23,6 @@
     </title>
     <?php wp_head(); ?>
 </head>
-<?php update_product_prices() ?>
-
 <body <?php body_class(); ?>>
     <div id="page" class="site">
         <header id="masthead" class="site-header">
@@ -55,7 +53,7 @@
                                                 <?php if (is_user_logged_in()) { ?>
                                                     Account
                                                 <?php } else { ?>
-                                                    Login
+                                                    Join
                                                 <?php } ?>
                                             </span>
                                         </a>
