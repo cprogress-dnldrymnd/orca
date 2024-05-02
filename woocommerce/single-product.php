50a036dc5ff4fe1b278b238cddebdb0c98a9799f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-02 22:24:12 +0800

    Update single-product.php

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -21,44 +21,45 @@ if (!defined('ABSPATH')) {
 }
 
 get_header('shop'); ?>
-
-<?php
-/**
- * woocommerce_before_main_content hook.
- *
- * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
- * @hooked woocommerce_breadcrumb - 20
- */
-do_action('woocommerce_before_main_content');
-?>
-
-<?php while (have_posts()) : ?>
-	<?php the_post(); ?>
-
+<section class="single-product">
 	<div class="container">
-		<?php wc_get_template_part('content', 'single-product'); ?>
+		<?php
+		/**
+		 * woocommerce_before_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+		 * @hooked woocommerce_breadcrumb - 20
+		 */
+		do_action('woocommerce_before_main_content');
+		?>
+
+		<?php while (have_posts()) : ?>
+			<?php the_post(); ?>
+
+			<?php wc_get_template_part('content', 'single-product'); ?>
+
+		<?php endwhile; // end of the loop. 
+		?>
+
+		<?php
+		/**
+		 * woocommerce_after_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+		 */
+		do_action('woocommerce_after_main_content');
+		?>
+
+		<?php
+		/**
+		 * woocommerce_sidebar hook.
+		 *
+		 * @hooked woocommerce_get_sidebar - 10
+		 */
+		do_action('woocommerce_sidebar');
+		?>
 	</div>
-
-<?php endwhile; // end of the loop. 
-?>
-
-<?php
-/**
- * woocommerce_after_main_content hook.
- *
- * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
- */
-do_action('woocommerce_after_main_content');
-?>
-
-<?php
-/**
- * woocommerce_sidebar hook.
- *
- * @hooked woocommerce_get_sidebar - 10
- */
-do_action('woocommerce_sidebar');
-?>
+</section>
 
 <?php
 get_footer('shop');
