7e36bc6e66363936a9f70731279816d47e298246
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-02 23:23:39 +0800

    Update header.php

--- header.php
+++ header.php
@@ -55,7 +55,7 @@
                                                 <?php if (is_user_logged_in()) { ?>
                                                     Account
                                                 <?php } else { ?>
-                                                    Join
+                                                    Login
                                                 <?php } ?>
                                             </span>
                                         </a>
