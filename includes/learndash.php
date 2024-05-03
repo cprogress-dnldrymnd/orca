b3e04dcf3aed4120bf9180e368340860b4c40507
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-03 19:22:57 +0800

    Update learndash.php

--- includes/learndash.php
+++ includes/learndash.php
@@ -107,7 +107,7 @@ function _learndash_status_bubble($id = NULL)
     } else {
         $id = $id;
     }
-    $course_status = learndash_course_status(get_the_ID(), get_current_user_id());
+    $course_status = learndash_course_status($id, get_current_user_id());
     return learndash_status_bubble($course_status, NULL, false);
 }
 add_shortcode('_learndash_status_bubble', '_learndash_status_bubble');
