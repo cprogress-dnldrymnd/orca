44918026dd623e57b3bbb7ab2f00b85cdda7ca67
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-03 17:00:28 +0800

    Update archive-sfwd-courses.php

--- archive-sfwd-courses.php
+++ archive-sfwd-courses.php
@@ -16,8 +16,7 @@ $args = array(
 
 $the_query = new WP_Query($args);
 $count = $the_query->found_posts;
-echo hide_load_more($count, 0, 10);
-
+echo hide_load_more($count, 0, 12);
 ?>
 <section class="archive-courses archive-grid archive-section background-light-gray py-5">
     <div class="container large-container">
