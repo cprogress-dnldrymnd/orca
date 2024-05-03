de5e3aeffc625885cca38524c36ee595fd523798
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-03 16:34:29 +0800

    Update post-types.php

--- includes/post-types.php
+++ includes/post-types.php
@@ -166,19 +166,11 @@ function movies_to_films($args, $post_type)
 }
 
 
-add_filter('pre_get_posts', 'custom_change_portfolio_posts_per_page');
-/**
- * Change Posts Per Page for Portfolio Archive.
- * 
- * @param object $query data
- *
- */
-function custom_change_portfolio_posts_per_page($query)
-{
-
-    if ($query->is_post_type_archive('sfwd-courses') && !is_admin() && $query->is_main_query()) {
-        $query->set('posts_per_page', 1);
+function action_pre_get_posts( $query ){
+    if( ! is_admin()
+        && $query->is_post_type_archive( 'sfwd-courses' )
+        && $query->is_main_query() ){
+            $query->set( 'posts_per_page', 5 );
     }
-
-    return $query;
 }
+add_action( 'pre_get_posts', 'action_pre_get_posts' );
