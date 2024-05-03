1d17b3a2222a61119df325ca7f3853289cb4f2ce
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-03 19:49:02 +0800

    Update learndash.php

--- includes/learndash.php
+++ includes/learndash.php
@@ -339,7 +339,7 @@ function _learndash_linked_product($atts)
         )
     );
 
-    $products = _learndash_has_linked_product(get_the_ID());
+    $products = _learndash_has_linked_product($id);
 
     $html = '';
 
@@ -354,7 +354,7 @@ function _learndash_linked_product($atts)
             if ($show_price == 'true') {
                 $html .= $product->get_price_html();
             }
-            $html .= '<a class="button add_to_cart_button"href="' . get_permalink(wc_get_page_id('shop')) . '?id="' . get_the_ID() . '"">  Add to cart </a>';
+            $html .= '<a class="button add_to_cart_button"href="' . get_permalink(wc_get_page_id('shop')) . '?id="' . $id . '"">  Add to cart </a>';
         }
     }
 
