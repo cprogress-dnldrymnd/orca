c654e756834ccca482b45a2995d4f60f28a53251
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-03 17:15:09 +0800

    Update archive-sfwd-courses.php

--- archive-sfwd-courses.php
+++ archive-sfwd-courses.php
@@ -85,7 +85,7 @@ echo hide_load_more($count, 0, $posts_per_page);
             </div>
         </div>
         <div class="load-more text-center mt-5 d-none">
-            <a href="#" id="load-more" class="d-none btn btn-accent">
+            <a href="#" id="load-more" class="btn btn-accent">
                 <span>Load more</span>
                 <svg class="spin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                     <path fill="currentColor" d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z" />
