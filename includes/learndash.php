6dda9fdfe2248f3ceeeeb14e100a5aa122002ff6
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-06 22:47:47 +0800

    Update learndash.php

--- includes/learndash.php
+++ includes/learndash.php
@@ -92,7 +92,11 @@ function _learndash_course_meta($atts)
             $html .= '<p"><strong>Price:</strong> ' . $price . '</p>';
         }
     } else {
-       
+        $product_p = wc_get_product($id);
+        $price_p = $product_p->get_price_html();
+        if ($price_p) {
+            $html .= '<p"><strong>Price:</strong> ' . $price_p . '</p>';
+        }
     }
     $html .= '</div>';
 
