29457330ca7cb49f6390c241084849383355c40c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-06 22:53:48 +0800

    Update learndash.php

--- includes/learndash.php
+++ includes/learndash.php
@@ -84,20 +84,18 @@ function _learndash_course_meta($atts)
         $html .= '<p class="d-none"><strong>Duration:</strong> 2 weeks</p>';
         $html .= '<p><strong>Certification:</strong> ' . $certification . '</p>';
     }
-
-    if (!_user_has_access($id) && $product_id && $post_type != 'product') {
-        $product = wc_get_product($product_id);
-        $price = $product->get_price_html();
-        if ($price) {
-            $html .= '<p"><strong>Price:</strong> ' . $price . '</p>';
-        }
+    if($post_type == 'product') {
+        echo 'product';
     } else {
-        $product_p = wc_get_product($id);
-        //$price_p = $product_p->get_regular_price();
-        if ($price_p) {
-            $html .= '<p"><strong>Price:</strong> ' . $id . '</p>';
+        if (!_user_has_access($id) && $product_id && $post_type != 'product') {
+            $product = wc_get_product($product_id);
+            $price = $product->get_price_html();
+            if ($price) {
+                $html .= '<p"><strong>Price:</strong> ' . $price . '</p>';
+            }
         }
     }
+  
     $html .= '</div>';
 
     return $html;
