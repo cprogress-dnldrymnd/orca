68381a02846effea780f1780147931778d39e653
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-10 22:39:05 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -62,6 +62,7 @@ Container::make('post_meta', 'Course Settings')
     Container::make('post_meta', 'Course Settings')
     ->where('post_type', '=', 'product')
     ->add_fields(array(
-            Field::make('hidden', '_ld_price_type', __('Price Type'))->set_default_value('paynow'),
+            Field::make('text', '_ld_price_type', __('Price Type'))->set_default_value('paynow')
+            ->set_attribute('readOnly', 'true'),
         
     ));
