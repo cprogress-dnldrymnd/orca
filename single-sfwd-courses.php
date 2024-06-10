eec0bbe450a5766c1f3eca8ba49ccced317f5b79
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-10 23:34:40 +0800

    Update single-sfwd-courses.php

--- single-sfwd-courses.php
+++ single-sfwd-courses.php
@@ -9,7 +9,7 @@ $args = array(
 $testimonials = get_posts($args);
 
 $groups = learndash_get_course_groups(get_the_ID());
-if ($groups && !current_user_can('administrator')) {
+if ($groups ) {
     $group_users = learndash_get_course_groups_users_access(get_the_ID());
     if (!in_array(get_current_user_id(), $group_users)) {
         wp_redirect(get_site_url() . '/courses');
