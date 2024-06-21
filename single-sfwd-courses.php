e2302a159222a7fe91f3ba69160c9fb89f7bc4b3
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-21 21:38:07 +0800

    Update single-sfwd-courses.php

--- single-sfwd-courses.php
+++ single-sfwd-courses.php
@@ -38,7 +38,7 @@ if ($groups && !current_user_can('administrator')) {
         var_dump($users_completed_the_course);
         echo '</pre>';
 
-        //carbon_set_post_meta(get_the_ID(), 'users_completed_the_course', $users_completed_the_course_arr);
+        carbon_set_post_meta(get_the_ID(), 'users_completed_the_course', $users_completed_the_course);
     }
     ?>
 
