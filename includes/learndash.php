476c6ed4b43e6353b8a64c05a65a1bafbef874cd
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-29 20:37:36 +0800

    Update learndash.php

--- includes/learndash.php
+++ includes/learndash.php
@@ -744,7 +744,8 @@ function _ld_certificate($atts)
             ),
             $atts
         )
-    );
+    );  
+    $ld_certificate = learndash_get_course_certificate_link(get_the_ID(), get_current_user_id());
     $html = '';
     $html .= '<div class="certificate-box">';
     $html .= '<div class="certificate-holder">';
@@ -763,6 +764,7 @@ function _ld_certificate($atts)
     $html .= "<p>$label<p>";
     $html .= '</div>';
 
+    
     $html .= '<div class="col-lg-5 text-end col-button">';
     $html .= '<a class="d-inline-flex justify-content-center" target="_blank" href="' . $ld_certificate . '"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16"> <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/> <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/> </svg><span>Download Certificate</span></a>';
     $html .= '</div>';
