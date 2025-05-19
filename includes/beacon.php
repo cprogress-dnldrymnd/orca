79868370a10baba8200809514aa60d81b18424a8
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-19 21:22:45 +0800

    Update beacon.php

--- includes/beacon.php
+++ includes/beacon.php
@@ -8,7 +8,7 @@ function beacon_api_function($api_url, $body, $method = 'PUT')
     );
     $response = wp_remote_get($api_url, array(
         'body' => $encoded_body,
-        'headers' =>   $headers,
+
         'method' => $method
     ));
 
