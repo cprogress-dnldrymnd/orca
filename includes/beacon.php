eb6e7c86415bc11562b65d383f9ffcede1d81bc7
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-06 17:39:16 +0800

    Update beacon.php

--- includes/beacon.php
+++ includes/beacon.php
@@ -229,9 +229,9 @@ function action_woocommerce_thankyou_test($order_id)
                 "notes" => 'Updated via woocommerce checkout'
             ],
         ];
-        $c_person = beacon_api_function('https://api.beaconcrm.org/v1/account/26878/entity/person/upsert', $body_create_person)['entity']['id'];
-
-        var_dump($c_person);
+        $beacon_api_function = beacon_api_function('https://api.beaconcrm.org/v1/account/26878/entity/person/upsert', $body_create_person);
+        $c_person = $beacon_api_function['entity']['id'];
+        var_dump($beacon_api_function);
         var_dump($body_create_person);
         update_user_meta($user_id, 'beacon_user_id', $c_person);
     } else {
