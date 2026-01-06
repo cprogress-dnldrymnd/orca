6aab24cf87e41d6b151bbd96a9adb2299f7042cc
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-06 18:25:53 +0800

    Update beacon.php

--- includes/beacon.php
+++ includes/beacon.php
@@ -10,7 +10,7 @@ function beacon_headers()
     );
 }
 
-function beacon_api_function($api_url, $body, $method = 'PUT')
+function beacon_api_function($api_url, $body, $method = 'PUT', $order_id = null)
 {
 
     $encoded_body = json_encode($body);
@@ -24,12 +24,20 @@ function beacon_api_function($api_url, $body, $method = 'PUT')
     if (is_wp_error($response)) {
         $error_message = $response->get_error_message();
         error_log("Something went wrong: $error_message");
+        add_beacon_crm_log("Failed Beacon API Response for order ID: $order_id", array(
+            'type' => 'Beacon Failed API Response',
+            'order_id' => $order_id
+        ));
         return false;
     } else {
         $body = wp_remote_retrieve_body($response);
         $data = json_decode($body, true);
         if ($data === null && json_last_error() !== JSON_ERROR_NONE) {
             error_log("Json decode error: " . json_last_error_msg());
+            add_beacon_crm_log("Failed Beacon API Response for order ID: $order_id", array(
+                'type' => 'Beacon Failed API Response',
+                'order_id' => $order_id
+            ));
             return false;
         }
         return $data;
@@ -38,79 +46,6 @@ function beacon_api_function($api_url, $body, $method = 'PUT')
 
 add_action('woocommerce_thankyou', 'action_woocommerce_thankyou', 10, 1);
 
-function action_woocommerce_thankyou($order_id)
-{
-
-    $order = wc_get_order($order_id);
-    $user_id = $order->get_user_id();
-    $beacon_user_id = get_user_meta($user_id, 'beacon_user_id', true);
-
-
-    $first_name = $order->get_billing_first_name();
-    $last_name  = $order->get_billing_last_name();
-    $email  = $order->get_billing_email();
-    $phone  = $order->get_billing_phone();
-    $address_1  = $order->get_billing_address_1();
-    $address_2  = $order->get_billing_address_2();
-    $city  = $order->get_billing_city();
-    $state  = $order->get_billing_state();
-    $postcode  = $order->get_billing_postcode();
-    $country  = $order->get_billing_country();
-    $items = $order->get_items();
-
-    if (!$beacon_user_id) {
-        $address = [
-            "address_line_one" => $address_1,
-            "address_line_two" => $address_2,
-            "city" => $city,
-            "region" => $state,
-            "postal_code" => $postcode,
-            "country" => WC()->countries->countries[$country],
-        ];
-
-        $body_create_person = [
-            "primary_field_key" => "emails",
-            "entity" => [
-                "emails" => [["email" => $email, "is_primary" => true]],
-                "phone_numbers" => [["number" => $phone, "is_primary" => true]],
-                "name" => [
-                    "full" => $first_name . ' ' . $last_name,
-                    "last" => $last_name,
-                    "first" => $first_name,
-                    "middle" => null,
-                    "prefix" => null,
-                ],
-                'type' => ['Supporter'],
-                "address" => [$address],
-                "notes" => 'Updated via woocommerce checkout'
-            ],
-        ];
-        $c_person = beacon_api_function('https://api.beaconcrm.org/v1/account/26878/entity/person/upsert', $body_create_person)['entity']['id'];
-        update_user_meta($user_id, 'beacon_user_id', $c_person);
-    } else {
-        $c_person = $beacon_user_id;
-    }
-
-    foreach ($items as $item) {
-        $product_id = $item->get_product_id();
-        $c_name = get_the_title($product_id) . " [Order ID: $order_id]";
-        $c_course = get__post_meta_by_id($product_id, 'beacon_id');
-        $c_course_type = get__post_meta_by_id($product_id, 'course_type');
-        if ($c_course && $c_course_type) {
-            $body_create_training = [
-                "primary_field_key" => "c_previous_db_id",
-                "entity" => [
-                    "c_person" => [intval($c_person)],
-                    "c_course" => [intval($c_course)],
-                    "c_course_type" => [$c_course_type],
-                    "c_previous_db_id" => $c_name
-                ]
-            ];
-            beacon_api_function('https://api.beaconcrm.org/v1/account/26878/entity/c_training/upsert', $body_create_training);
-        }
-    }
-    beacon_create_payment($order_id);
-}
 
 function beacon_create_payment($order_id)
 {
@@ -154,7 +89,7 @@ function beacon_create_payment($order_id)
                     'notes' => 'Payment made via woocommerce checkout for course: ' . $c_name,
                 ],
             ];
-            beacon_api_function('https://api.beaconcrm.org/v1/account/26878/entity/payment/upsert', $body_create_payment, 'PUT');
+            beacon_api_function('https://api.beaconcrm.org/v1/account/26878/entity/payment/upsert', $body_create_payment, 'PUT', $order_id);
             add_beacon_crm_log("Created Beacon Payment for user ID: $user_id", array(
                 'type' => 'Beacon Training',
                 'user_id' => $user_id,
@@ -241,7 +176,7 @@ function action_woocommerce_thankyou_test($order_id)
             $body_create_person['entity']['phone_numbers'] = [["number" => $phone, "is_primary" => true]];
         }
 
-        $beacon_api_function = beacon_api_function('https://api.beaconcrm.org/v1/account/26878/entity/person/upsert', $body_create_person);
+        $beacon_api_function = beacon_api_function('https://api.beaconcrm.org/v1/account/26878/entity/person/upsert', $body_create_person, $order_id);
         $c_person = $beacon_api_function['entity']['id'];
         var_dump($beacon_api_function);
 
@@ -252,8 +187,6 @@ function action_woocommerce_thankyou_test($order_id)
             'beacon_person_id' => $c_person,
             'order_id' => $order_id
         ));
-
-      
     } else {
         $c_person = $beacon_user_id;
     }
@@ -273,7 +206,7 @@ function action_woocommerce_thankyou_test($order_id)
                     "c_previous_db_id" => $c_name
                 ]
             ];
-            beacon_api_function('https://api.beaconcrm.org/v1/account/26878/entity/c_training/upsert', $body_create_training);
+            beacon_api_function('https://api.beaconcrm.org/v1/account/26878/entity/c_training/upsert', $body_create_training, $order_id);
             add_beacon_crm_log("Created Beacon Training for user ID: $user_id", array(
                 'type' => 'Beacon Training',
                 'user_id' => $user_id,
